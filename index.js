27c33a205b19cc21cbedc2500e7e803691fe8868
Caleeeb <daycinematography@gmail.com>
2022-05-04 14:49:21 -0400

    psuedo-code

---
+++ index.js
@@ -0,0 +1,18 @@
+// npm packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
+
+// class definitions for each position
+
+// declare an empty array for team members
+
+// inquirer prompts about each team member to create class:
+    // manager prompts
+        // engineer or intern prompts or finish application.
+            // use a switch or prompt question into if statement in 'then'???? 
+    // then in class creation
+
+// generate html file
+
+// call function to initiate application
