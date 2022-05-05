ea050f8600bf24b4562401ce8d566cbc0b870e6a
Caleeeb <daycinematography@gmail.com>
2022-05-04 22:26:36 -0400

    Manager test is working woohoo

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,31 +3,31 @@ const Employee = require("../lib/Employee");
 let nameExample = "Kenneth Lay";
 let idExample = "0001";
 let emailExample = "klay@enron.com";
-const employee = new Employee(nameExample, idExample, emailExample);
+const employeeExample = new Employee(nameExample, idExample, emailExample);
 
 describe("Employee", () => {
     it("should be an object with values name, id, and email", () => {
 
-        expect(employee.name).toEqual("Kenneth Lay");
-        expect(employee.id).toEqual("0001");
-        expect(employee.email).toEqual("klay@enron.com");
+        expect(employeeExample.name).toEqual("Kenneth Lay");
+        expect(employeeExample.id).toEqual("0001");
+        expect(employeeExample.email).toEqual("klay@enron.com");
     });
 });
 
 describe("getName", () => {
     it("should return the name of the employee", () => {
-        expect(employee.getName()).toEqual("Kenneth Lay");
+        expect(employeeExample.getName()).toEqual("Kenneth Lay");
     });
 });
 
 describe("getId", () => {
     it("should return the id of the employee", () => {
-        expect(employee.getId()).toEqual("0001");
+        expect(employeeExample.getId()).toEqual("0001");
     });
 });
 
 describe("getRole", () => {
     it("should return the role of the employee", () => {
-        expect(employee.getRole()).toEqual('Employee');
+        expect(employeeExample.getRole()).toEqual('Employee');
     });
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,20 +4,20 @@ let nameExample = "Elizabeth Holmes";
 let idExample = "0001";
 let emailExample = "eholmes@theranos.com";
 let officeNumberExample = "666";
-const manager = new Manager(nameExample, idExample, emailExample, officeNumberExample);
+const managerExample = new Manager(nameExample, idExample, emailExample, officeNumberExample);
 
 describe("Manager", () => {
     it("should create an object with values name, id, email, and office number", () => {
-        expect(manager.name).toEqual("Elizabeth Holmes");
-        expect(manager.id).toEqual("0001");
-        expect(manager.email).toEqual("eholmes@theranos.com");
-        expect(manager.officeNumber).toEqual("666");
+        expect(managerExample.name).toEqual("Elizabeth Holmes");
+        expect(managerExample.id).toEqual("0001");
+        expect(managerExample.email).toEqual("eholmes@theranos.com");
+        expect(managerExample.officeNumber).toEqual("666");
     });
 });
 
 describe("getRole", () => {
     it("should return the 'Manager'", () => {
-        expect(manager.getRole).toEqual("Manager");
+        expect(managerExample.getRole()).toEqual("Manager");
     });
 });
 
