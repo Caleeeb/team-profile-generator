f5b9480f5a6ef65967265357b6cca8b0bb02e093
Caleeeb <daycinematography@gmail.com>
2022-05-04 23:17:37 -0400

    intern and engineer prompts

--- index.js
+++ index.js
@@ -28,21 +28,81 @@ const addManager = () => {
         { 
             type: 'input',
             message: "What is the manager's employee ID?",
-            name: 'managerID'
+            name: 'managerId'
         },
         { 
             type: 'input',
             message: "What is the manager's office number?",
             name: 'managerOfficeNumber'
         },
+        {
+            type: 'confirm',
+            message: "Would you like to add another team member?",
+            name: 'addEmployee',
+            default: true,
+        }
+
     // destructure responses
-    ]).then(({managerName, managerEmail, managerID, managerOfficeNumber} = response) => {
+    ]).then(({managerName, managerEmail, managerId, managerOfficeNumber, addEmployee} = response) => {
         // push specific to create manager class
-        workforce.push(new Manager(managerName, managerEmail, managerID, managerOfficeNumber));
+        workforce.push(new Manager(managerName, managerEmail, managerId, managerOfficeNumber));
+        // engineer or intern prompts or finish application.
+        // user selects to add a new team member
+        if (addEmployee === true) {
         addTeamMember();
+        }
+        // when user is done adding team members
+        else {
+            createHTML();
+        }
     }) 
 }
-        // engineer or intern prompts or finish application.
+// prompts to get data for Engineer and Intern classes
+const addTeamMember = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: "What is the position this team member?",
+            name: 'employeeRole',
+            editableList: false,
+            choice: ['Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            message: "What is the team member's name?",
+            name: 'employeeName'
+        },
+        {
+            type: 'input',
+            message: "What is the team member's ID number?",
+            name: 'employeeId'
+        },
+        {
+            type: 'input',
+            message: "What is the team member's email address?",
+            name: 'employeeEmail'
+        },
+        {
+            type: 'input',
+            message: "What is this engineer's GitHub username?",
+            name: 'engineerGitHub',
+            // use 'when' so certain prompts are shown based on which employee role is chosen by user
+            when: answers => answers.employeeRole == 'Intern'
+        },
+        {
+            type: 'input',
+            message: "What school does this intern attend?",
+            name: 'internSchool',
+            when: answers => answers.employeeRole == 'Engineer'
+        },
+        {
+            type: 'confirm',
+            message: "Would you like to add another team member?",
+            name: 'addEmployee',
+            default: 'true'
+        },
+    ])
+}
             // use a switch or prompt question into if statement in 'then'???? 
     // then in class creation
 
