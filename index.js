c656e5500fb12e49608de369d8c12f4692a10628
Caleeeb <daycinematography@gmail.com>
2022-05-05 00:09:05 -0400

    html function started

--- index.js
+++ index.js
@@ -15,22 +15,22 @@ let workforce = [];
 // manager prompts
 const addManager = () => {
     return inquirer.prompt([
-        { 
+        {
             type: 'input',
             message: "What is the manager's name?",
             name: 'managerName'
         },
-        { 
+        {
             type: 'input',
             message: "What is the manager's email?",
             name: 'managerEmail'
         },
-        { 
+        {
             type: 'input',
             message: "What is the manager's employee ID?",
             name: 'managerId'
         },
-        { 
+        {
             type: 'input',
             message: "What is the manager's office number?",
             name: 'managerOfficeNumber'
@@ -42,20 +42,20 @@ const addManager = () => {
             default: true,
         }
 
-    // destructure responses
-    ]).then(({managerName, managerEmail, managerId, managerOfficeNumber, addEmployee} = response) => {
+        // destructure responses
+    ]).then(({ managerName, managerEmail, managerId, managerOfficeNumber, addEmployee } = response) => {
         // push specific to create manager class
         workforce.push(new Manager(managerName, managerEmail, managerId, managerOfficeNumber));
         // engineer or intern prompts or finish application.
         // user selects to add a new team member
         if (addEmployee === true) {
-        addTeamMember();
+            addTeamMember();
         }
         // when user is done adding team members
         else {
             createHTML();
         }
-    }) 
+    })
 }
 // prompts to get data for Engineer and Intern classes
 const addTeamMember = () => {
@@ -102,8 +102,45 @@ const addTeamMember = () => {
             default: 'true'
         },
     ])
+        .then(({ employeeRole, employeeName, employeeId, employeeEmail, engineerGitHub, internSchool, addEmployee } = addMember) => {
+            // if chosen, create engineer class constructor
+            if (employeeRole == "Engineer") {
+                workforce.push(new Engineer(employeeId, employeeName, employeeEmail, engineerGitHub));
+            }
+            // if chosen, create intern class constructor
+            else if (employeeRole == "Intern") {
+                workforce.push(new Intern(employeeId, employeeName, employeeEmail, internSchool));
+            }
+            // if user chose to make another team member, restart team member prompts
+            if (addEmployee === true) {
+                addTeamMember();
+            }
+            else {
+                createHTML();
+            }
+        })
 }
-            // use a switch or prompt question into if statement in 'then'???? 
+// function that creates HTML file
+function createHTML() {
+    fs.writeFile('./dist/teamprofile.html',
+        `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header class=""main_header bg-red text-white>The Team</header>
+        <section class=""main_container"></section> 
+    </body>
+    </html>
+    `
+    );
+}
     // then in class creation
 
 // generate html file
