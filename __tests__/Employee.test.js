e7d42ae590d8d260107d757453c1a7203ef99093
Caleeeb <daycinematography@gmail.com>
2022-05-04 21:51:45 -0400

    Employee test fully finished

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,14 +1,32 @@
 const Employee = require("../lib/Employee");
+let nameExample = "Kenneth Lay";
+let idExample = "0001";
+let emailExample = "klay@enron.com";
+const employee = new Employee(nameExample, idExample, emailExample);
 
 describe("Employee", () => {
     it("should be an object with values name, id, and email", () => {
-        let nameExample = "Kanneth Lay";
-        let idExample = "0001";
-        let emailExample = "klay@enron.com";
-        const employee = new Employee(nameExample, idExample, emailExample);
 
-        expect(employee.name).toEqual("Kanneth Lay");
+        expect(employee.name).toEqual("Kenneth Lay");
         expect(employee.id).toEqual("0001");
         expect(employee.email).toEqual("klay@enron.com");
+    });
+});
+
+describe("getName", () => {
+    it("should return the name of the employee", () => {
+        expect(employee.getName()).toEqual("Kenneth Lay");
+    });
+});
+
+describe("getId", () => {
+    it("should return the id of the employee", () => {
+        expect(employee.getId()).toEqual("0001");
+    });
+});
+
+describe("getRole", () => {
+    it("should return the role of the employee", () => {
+        expect(employee.getRole()).toEqual('Employee');
     });
 });
