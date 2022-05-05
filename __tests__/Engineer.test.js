431cef0ad3ffd79f6f73534878f52303454eece1
Caleeeb <daycinematography@gmail.com>
2022-05-05 00:57:59 -0400

    engineer test completed

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,30 @@
+const Engineer = require('../lib/Engineer');
+
+let nameExample = "Alexey Pazhitnov";
+let idExample = "4444";
+let emailExample = "apazhitnov@tetris.com";
+let githubExample = "TetrisMaster";
+const engineerExample = new Engineer(nameExample, idExample, emailExample, githubExample);
+
+describe("engineer", () => {
+    it("should be an object with values name, id, email and github", () => {
+        expect(engineerExample.name).toEqual("Alexey Pazhitnov");
+        expect(engineerExample.id).toEqual("4444");
+        expect(engineerExample.email).toEqual("apazhitnov@tetris.com");
+        expect(engineerExample.github).toEqual("TetrisMaster");
+    });
+});
+
+describe("getGithub", () => {
+    it("should return github username", () => {
+        expect(engineerExample.getGithub()).toEqual("TetrisMaster");
+    });
+});
+
+describe("getRole", () => {
+    it('should return role of the engineer', () => {
+        expect(engineerExample.getRole()).toEqual('Engineer');
+    });
+});
+
+
